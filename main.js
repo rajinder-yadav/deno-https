2e1e801339d9f5b1f49d1a2fb9c637e1e5e519d5
Dev Guy <devguy.ca@gmail.com>
2025-03-15 20:22:37 -0400

    feat: demo app

---
+++ main.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const fs = require('fs');
+
+// Read the certificate and private key
+const options = {
+  key: fs.readFileSync('private-key.pem', 'utf8'),
+  cert: fs.readFileSync('certificate.pem', 'utf8'),
+  passphrase: 'hello123' // Replace with your actual passphrase
+};
+
+// Create the HTTPS server
+https.createServer(options, (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, this is an HTTPS server with an encrypted private key!\n');
+}).listen(3333, () => {
+  console.log('HTTPS server running on port 3333');
+});
